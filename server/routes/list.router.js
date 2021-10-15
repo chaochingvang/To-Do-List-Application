ec95dd82f84facafe56844a6d151937120e2aadb
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-15 16:03:03 -0500

    created GET request from router to db, successful test with postman

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,5 +5,23 @@ const pool = require(`../modules/pool.js`);
 
 console.log(`IN list.router.js`);
 
+router.get(`/`, (req, res) => {
+    console.log(`in /list router get request`);
+
+    let queryText = `
+        SELECT * FROM "toDoList";
+    `;
+
+    pool.query(queryText)
+        .then((result) => {
+            console.log(`sending list from db success!`);
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log(`Error! Unable to send list from db.`);
+            res.sendStatus(500);
+        });
+});
+
 
 module.exports = router;
